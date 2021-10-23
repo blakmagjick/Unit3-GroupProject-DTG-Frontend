6a82c29343b33db9aa2c288ae7d1839015b723a4
Suzy Q <suzy@iamsuzyq.com>
2021-10-23 12:09:28 -0700

    NewUser component added

---
+++ dtg-frontend/src/NewUser.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class NewUser extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <h2>Ready Player One?</h2>
+            </React.Fragment>
+        )
+    }
+}
