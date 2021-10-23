800d9e6033f692b93dfe7f7adbc32eef0feb3640
Suzy Q <suzy@iamsuzyq.com>
2021-10-23 13:40:23 -0700

    NewGamer form working

--- dtg-frontend/src/App.js
+++ dtg-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './About'
@@ -7,22 +7,60 @@ import Header from './Header'
 import Navigation from './Navigation'
 import AllGamers from './AllGamers'
 import Login from './Login'
+import NewUser from './NewUser'
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route path="/about" exact component={() => <About />} />
-          <Route path="/login" exact component={() => <Login />} />
-          <Route path="/gamers" exact component={() => <AllGamers />} />
-        </Switch>
-      </Router>
-      <Footer />
-    </React.Fragment>
-  );
+let baseURL = 'http://localhost:3003'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      gamers: null
+    }
+  }
+
+  getGamers = () => {
+    fetch(baseURL + '/gamers')
+    .then(response => {
+      return response.json()
+      }) 
+    .then(data => {
+      console.log(data)
+      this.setState({
+        gamers: data
+      })
+    })
+  }
+
+  addGamer = (newGamer) => {
+    const copyGamers = [...this.state.gamers]
+    copyGamers.push(newGamer)
+    this.setState({
+      gamers: copyGamers
+    })
+  }
+
+  componentDidMount(){
+    this.getGamers()
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route path="/about" exact component={() => <About />} />
+            <Route path="/login" exact component={() => <Login />} />
+            <Route path="/gamers" exact component={() => <AllGamers />} />
+          </Switch>
+        </Router>
+        <NewUser baseURL={baseURL} addGamer={this.addGamer}/>
+        <Footer />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
--- dtg-frontend/src/NewUser.js
+++ dtg-frontend/src/NewUser.js
@@ -4,14 +4,69 @@ export default class NewUser extends Component {
     constructor(props){
         super(props)
         this.state = {
-
+            name: '',
+            profilePic: '',
+            age: '',
+            location: '',
+            faveGames: ''
         }
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value
+        this.setState({
+            [event.target.name]: value
+        })
+      }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        fetch(this.props.baseURL + '/gamers', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: this.state.name,
+                profilePic: this.state.profilePic,
+                age: this.state.age,
+                location: this.state.location,
+                faveGames: this.state.faveGames
+            }),
+            headers: {
+            'Content-Type': 'application/json'
+            },
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then( data => {
+            console.log(data)
+            this.props.addGamer(data)
+            this.setState({
+                name: '',
+                profilePic: '',
+                age: '',
+                location: '',
+                faveGames: ''
+            })
+        }).catch (error => console.error({'Error': error}))
     }
 
     render(){
         return(
             <React.Fragment>
                 <h2>Ready Player One?</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' id='name' name='name' onChange={(event) => this.handleChange(event)}></input><br />
+                    <label htmlFor='profilePic'>Avatar:</label>
+                    <input type='text' id='profilePic' name='profilePic' onChange={(event) => this.handleChange(event)}></input><br />
+                    <label htmlFor='age'>Age:</label>
+                    <input type='number' id='age' name='age' onChange={(event) => this.handleChange(event)}></input><br />
+                    <label htmlFor='location'>Location:</label>
+                    <input type='text' id='location' name='location' onChange={(event) => this.handleChange(event)}></input><br />
+                    <label htmlFor='faveGames'>Fave Games:</label>
+                    <input type='text' id='faveGames' name='faveGames' onChange={(event) => this.handleChange(event)}></input><br />
+                    <input type='submit' value='Create Profile' />
+                </form>
             </React.Fragment>
         )
     }
