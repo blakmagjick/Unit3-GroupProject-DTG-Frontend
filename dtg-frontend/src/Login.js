6d421568fa12627ae4daf7cfbbb2007093bc83ce
nazdak504 <aguiluz.matt@gmail.com>
2021-10-23 13:18:21 -0500

    login changed to class component

--- dtg-frontend/src/Login.js
+++ dtg-frontend/src/Login.js
@@ -1,30 +1,46 @@
-function Login (props) {
-    return(
-        <div className ='login'>
-            <div class='container'>
-                <div class='row align-items-center my-5'>
-                    <form>
-                    <strong>Login</strong>
-                        <label htmlFor='name'>Username: </label>
-                        <input type ='text' id='name' name='username'/>
-                        <label htmlFor='name'>Password: </label>
-                        <input type='text' id='passowrd' name='password'/>
-                        <input type='submit' value='login' />
-                    </form>
-                    OR
-                    <form>
-                        <strong>Register</strong>
-                        <label htmlFor='name'>Username:</label>
-                        <input type='text' id='name' name='username'/>
-                        <label htmlFor='name'>Password: </label>
-                        <input type='text' id='password' name='password'/>
-                        <input type='submit'
-                        value='signup' />                    
-                    </form>
+import React, { Component } from 'react'
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            password: '',
+        }
+
+    }
+
+
+    render(){
+        return(
+            <div className ='login'>
+                <div className='container'>
+                    <div className='row align-items-center my-5'>
+                        <form>
+                        <strong>Login</strong>
+                            <label htmlFor='name'>Username: </label>
+                            <input type ='text' id='name' name='username'/>
+                            <label htmlFor='name'>Password: </label>
+                            <input type='text' id='passowrd' name='password'/>
+                            <input type='submit' value='login' />
+                        </form>
+                        OR
+                        <form>
+                            <strong>Register</strong>
+                            <label htmlFor='name'>Username:</label>
+                            <input type='text' id='name' name='username'/>
+                            <label htmlFor='name'>Password: </label>
+                            <input type='text' id='password' name='password'/>
+                            <input type='submit'
+                            value='signup' />                    
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Login
