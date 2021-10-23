673dd54bc84153e86ae5cf3e443eca7a5db7966c
nazdak504 <aguiluz.matt@gmail.com>
2021-10-23 13:59:38 -0500

    login functionality added

--- dtg-frontend/src/Login.js
+++ dtg-frontend/src/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+let baseUrl = 'http://localhost:3003'
+
 class Login extends Component {
 
     constructor(props) {
@@ -8,8 +10,67 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
+            userLoggedIn: false,
         }
+    }
 
+    // login 
+    loginUser = async (e) => {
+        console.log('loginUser')
+        e.preventDefault()
+
+        const url = baseUrl + '/users/login'
+        const loginBody = {
+            username: e.target.username.value,
+            password: e.target.password.value
+        }
+        try {
+            
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(loginBody),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include'
+            })
+
+            console.log(response)
+            console.log('BODY: ', response.body)
+
+            if(response.status === 200) {
+                // this.getProfiles()
+                console.log('🌈 login successful!🌈')
+            }
+        }
+        catch (error) {
+            console.log('Error => ', error)
+        }
+    }
+
+    // signup
+    signup = async (e) => {
+        e.preventDefault()
+        const url = baseUrl + '/users/signup'
+        try {
+            const response = await fetch(url, {
+                method: 'POST', 
+                body: JSON.stringify({
+                    username: e.target.username.value,
+                    password: e.target.password.value
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (response.status === 200) {
+                // this.getProfiles()
+                console.log('🏄‍♀️ singup successful! 🏄‍♂️')
+            }
+        }
+        catch (error) {
+            console.log('Error => ', error)
+        }
     }
 
 
@@ -18,19 +79,19 @@ class Login extends Component {
             <div className ='login'>
                 <div className='container'>
                     <div className='row align-items-center my-5'>
-                        <form>
+                        <form onSubmit={this.loginUser}>
                         <strong>Login</strong>
-                            <label htmlFor='name'>Username: </label>
-                            <input type ='text' id='name' name='username'/>
+                            <label htmlFor='username'>Username: </label>
+                            <input type ='text' id='username' name='username'/>
                             <label htmlFor='name'>Password: </label>
-                            <input type='text' id='passowrd' name='password'/>
+                            <input type='text' id='password' name='password'/>
                             <input type='submit' value='login' />
                         </form>
                         OR
-                        <form>
+                        <form onSubmit={this.signup}>
                             <strong>Register</strong>
-                            <label htmlFor='name'>Username:</label>
-                            <input type='text' id='name' name='username'/>
+                            <label htmlFor='username'>Username:</label>
+                            <input type='text' id='username' name='username'/>
                             <label htmlFor='name'>Password: </label>
                             <input type='text' id='password' name='password'/>
                             <input type='submit'
