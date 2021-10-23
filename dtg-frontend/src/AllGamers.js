8353f366bcad2f6e2256916b0d4fc5f9d7f3246e
Suzy Q <suzy@iamsuzyq.com>
2021-10-23 10:30:29 -0700

    Tested and got multi react routes working in Nav bar

---
+++ dtg-frontend/src/AllGamers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AllGamers() {
+    return (
+        <React.Fragment>
+            <h2>All Gamers</h2>
+        </React.Fragment>
+    )
+}
+
+export default AllGamers
