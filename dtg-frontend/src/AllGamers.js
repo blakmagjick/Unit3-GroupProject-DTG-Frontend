a2b96663f95dfe38b22693accfcb6da73d883b93
nazdak504 <aguiluz.matt@gmail.com>
2021-10-26 22:37:25 -0500

    and now with searchgamer.js added to index page

--- dtg-frontend/src/AllGamers.js
+++ dtg-frontend/src/AllGamers.js
@@ -23,6 +23,7 @@ export default class AllGamers extends Component {
                             )
                         })
                     }
+                <SearchGamer />
                 </React.Fragment>
             )
         } else {
